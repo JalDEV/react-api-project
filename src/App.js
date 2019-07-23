742282d161fd7f72ae50be40bd3be7d56e612fcf
Jeff Losee <macbook3@Avant8s-MacBook-Pro-2.local>
2019-07-23 17:37:51 -0600

    PART I - Basic Data Retrieval

--- src/App.js
+++ src/App.js
@@ -6,19 +6,45 @@ import star from './images/star.svg';
 import wars from './images/wars.svg';
 
 class App extends Component {
-  render() {
-    return (
-      <div className='content'>
-        <div className='logo'>
-          <img src={star} alt="star-logo" />
-          <span className='interview-text'>The Interview</span>
-          <img src={wars} alt="wars-logo" />
-        </div>
-        <SearchBar />
-        <Card />
-      </div>
-    );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      people: null,
+      planets: null
+    };
   }
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      console.log(this.state.planets);
+      
+      return (
+        <div>
+          <SearchBar />
+          {this.state.people.map(person => (
+            <Card key={person.name} person={person} name={person.name}  planet={this.state.planets[person.homeworld - 1].name} birthday={person.birth_year}  />
+          ), this)}
+        </div>
+      );
+    }
+  }
+  
+  componentDidMount() {
+    fetch('http://localhost:3008/people')
+      .then(response => response.json())
+      .then(data => this.setState({ people: data }));
+
+    fetch('http://localhost:3008/planets')
+    .then(response => response.json())
+    .then(data => this.setState({ isLoaded: true, planets: data }));
+  }
+
+  
 }
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -2,20 +2,22 @@ import React, { Component } from 'react';
 import './Card.css';
 
 class Card extends Component {
+
   render() {
+    console.log(this.props.planet);
     return (
       <div className='card'>
         <div className='card-content'>
-          	<div className='card-name'>Darth Vader</div>
-          	<img src='http://localhost:3008/darth_vader.jpg' alt='profile'/>
+          	<div className='card-name'>{this.props.name}</div>
+          	<img src={'http://localhost:3008/' + this.props.person.image} alt='profile'/>
             <p>
                 <span>Birthday:</span>
-                <span>41.9BBY</span>
+                <span>{this.props.birthday}</span>
             </p>
             <p>
                 {/* Note that in order to get the homeworld's name, you have to get the planet name from a different endpoint than the people */}
                 <span>Homeworld:</span>
-                <span>Tatooine</span>
+                <span>{this.props.planet}</span>
             </p>
         </div>
     </div>
